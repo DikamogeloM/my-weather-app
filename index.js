0b683b9aa463b0c34b97e7b8feb9d568e357c25b
DikamogeloM <dikamogelom@gmail.com>
2024-06-05 00:16:34 +0200

    Integrated the weather API inside Javascript

--- index.js
+++ index.js
@@ -56,32 +56,45 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [`Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "4fft9ab4oba0fa5c5b989ee6eb4b9143";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
-  console.log(apiUrl);
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = [`Thu`, `Fri`, `Sat`, `Sun`, `Mon`];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌒</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div>
+            <img src= "${
+              day.condition.icon_url
+            }" class="weather-forecast-icon"/>
+            </div>
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max">24° </span>
-              <span class="weather-forecast-temperature-min">12°</span>
+              <span class="weather-forecast-temperature-max">${Math.round(
+                day.temperature.maximum
+              )}°  </span>
+              <span class="weather-forecast-temperature-min">${Math.round(
+                day.temperature.minimum
+              )}°</span>
             </div>
           </div>
           `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
